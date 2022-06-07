02c1dac1772f1ec17b88d39f74724757690333b2
Josef Friedrich <josef@friedrich.rocks>
2022-06-07 08:53:58 +0200

    Add some docs

---
+++ examples/definitions/all-options.lua
@@ -0,0 +1,49 @@
+require('busted.runner')()
+local luakeys = require('luakeys')
+
+-- Update this code example in the README.md
+local defintions = {
+  key = {
+    -- Allow different key names.
+    -- or a single string: alias = 'k'
+    alias = { 'k', 'ke' },
+
+    -- The key is always included in the result. If no default value is
+    -- definied, true is taken as the value.
+    always_present = false,
+
+    -- Only values listed in the array table are allowed.
+    choices = { 'one', 'two', 'three' },
+
+    -- Possible data types: boolean, dimension, integer, number, string
+    data_type = 'string',
+
+    default = true,
+
+    -- The key belongs to a mutually exclusive group of keys.
+    exclusive_group = 'name',
+
+    -- > \g_my_token_list_tl
+    l3_tl_set = 'my_token_list',
+
+    -- > \MacroName
+    macro = 'MacroName', -- > \MacroName
+
+    -- See http://www.lua.org/manual/5.3/manual.html#6.4.1
+    match = '^%d%d%d%d%-%d%d%-%d%d$',
+
+    -- The name of the key, can be omitted
+    name = 'key',
+    opposite_keys = { [true] = 'show', [false] = 'hide' },
+    process = function(value, input, result, unknown)
+      return value
+    end,
+    required = true,
+    sub_keys = { key_level_2 = { } },
+  }
+}
+
+local parse = luakeys.define(defintions)
+assert.has_error(function ()
+  parse('key=one')
+end)
