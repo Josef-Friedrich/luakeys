771b82dbd2587c9b58311b6d698fdd0a4d15aee2
Josef Friedrich <josef@friedrich.rocks>
2022-06-08 20:19:08 +0200

    Update docs

--- examples/functions/debug.lua
+++ examples/functions/debug.lua
@@ -3,3 +3,7 @@ local luakeys = require('luakeys')
 
 local result = luakeys.parse('level1={level2={key=value}}')
 luakeys.debug(result)
+
+it('result', function()
+  assert.is.same(result, { level1 = { level2 = { key = 'value' } } })
+end)
---
+++ examples/functions/define.lua
@@ -0,0 +1,41 @@
+require('busted.runner')()
+local luakeys = require('luakeys')
+
+-- standalone string values
+local defs = { 'key' }
+
+-- keys in a Lua table
+local defs = { key = {} }
+
+-- by the “name” attribute
+local defs = { { name = 'key' } }
+
+local parse = luakeys.define(defs)
+local result, unknown = parse('key=value,unknown=unknown', { no_error = true })
+luakeys.debug(result) -- { key = 'value' }
+luakeys.debug(unknown) -- { unknown = 'unknown' }
+
+it('result', function()
+  assert.is.same(result, { key = 'value' })
+end)
+
+it('unknown', function()
+  assert.is.same(unknown, { unknown = 'unknown' })
+end)
+
+local parse2 = luakeys.define({
+  level1 = {
+    sub_keys = { level2 = { sub_keys = { key = { some_def = 'etc' } } } },
+  },
+}, { no_error = true })
+local result2, unknown2 = parse2('level1={level2={key=value,unknown=unknown}}')
+luakeys.debug(result2) -- { level1 = { level2 = { key = 'value' } } }
+luakeys.debug(unknown2) -- { level1 = { level2 = { unknown = 'unknown' } } }
+
+it('result2', function()
+  assert.is.same(result2, { level1 = { level2 = { key = 'value' } } })
+end)
+
+it('unknown2', function()
+  assert.is.same(unknown2, { level1 = { level2 = { unknown = 'unknown' } } })
+end)
