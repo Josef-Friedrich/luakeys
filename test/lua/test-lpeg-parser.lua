b3566c7b469339c52e33b0f705591627e0fef4d0
Josef Friedrich <josef@friedrich.rocks>
2022-06-08 12:34:52 +0200

    Consolidate tests

--- test/lua/test-lpeg-parser.lua
+++ test/lua/test-lpeg-parser.lua
@@ -2,17 +2,24 @@ require('busted.runner')()
 
 local luakeys = require('luakeys')
 
+local function assert_result(kv_string, expected, opts)
+  assert.are.same(expected, luakeys.parse(kv_string, opts))
+end
+
+local function assert_value(actual, expected)
+  local result = luakeys.parse('key=' .. actual)
+  local input = result.key
+  assert.are.equal(expected, input)
+end
+
 describe('LPeg Parser', function()
-
-  local assert_value = function(actual, expected)
-    local result = luakeys.parse('key=' .. actual)
-    local input = result.key
-    assert.are.equal(expected, input)
-  end
-
   describe('Lua number', function()
-    it('Integer: 1', function()
+    it('Integer', function()
       assert_value('1', 1)
+    end)
+
+    it('Integer with leading zeros', function()
+      assert_value('0001', 1)
     end)
 
     it('Integer with a plus sign: +1', function()
@@ -220,36 +227,170 @@ describe('LPeg Parser', function()
     end)
 
     describe('Keys', function()
-      local function assert_deep_equals(actual, expected)
-        assert.are.same(expected,
-          luakeys.parse(actual, { naked_as_value = true }))
-      end
-
       it('Simple string', function()
-        assert_deep_equals('key=1', { key = 1 })
+        assert_result('key=1', { key = 1 })
       end)
 
       it('string: Umlaute', function()
-        assert_deep_equals('umlaute öäü=1', { ['umlaute öäü'] = 1 })
+        assert_result('umlaute öäü=1', { ['umlaute öäü'] = 1 })
       end)
 
       it('string: underscore: “under_score=1”', function()
-        assert_deep_equals('1=a,b', { 'a', 'b' })
+        assert_result('under_score=1', { under_score = 1 })
       end)
 
       it('number: “2=2”', function()
-        assert_deep_equals('2=2', { [2] = 2 })
+        assert_result('2=2', { [2] = 2 })
       end)
 
       it('number: “1=a,b”', function()
-        assert_deep_equals('1=a,b', { 'a', 'b' })
+        assert_result('1=a,b', { 'a', 'b' }, { naked_as_value = true })
       end)
     end)
   end)
 
   describe('Nested', function()
     it('single value nested', function()
-      assert.are.same({ key = { 1 } }, luakeys.parse('key={1}', { unpack = false }))
+      assert.are.same({ key = { 1 } },
+        luakeys.parse('key={1}', { unpack = false }))
     end)
+  end)
+end)
+
+describe('Whitespaces', function()
+  it('No whitepsaces', function()
+    assert_result('integer=1', { integer = 1 })
+  end)
+
+  it('With whitespaces', function()
+    assert_result('integer = 2', { integer = 2 })
+  end)
+
+  it('With tabs', function()
+    assert_result('integer\t=\t3', { integer = 3 })
+  end)
+
+  it('With newlines', function()
+    assert_result('integer\n=\n4', { integer = 4 })
+  end)
+
+  it('With whitespaces, tabs and newlines', function()
+    assert_result('integer \t\n= \t\n5 , boolean=false',
+      { integer = 5, boolean = false })
+  end)
+
+  it('Two keys with whitespaces', function()
+    assert_result('integer=1 , boolean=false', { integer = 1, boolean = false })
+  end)
+
+  it('Two keys with whitespaces, tabs, newlines', function()
+    assert_result('integer \t\n= \t\n1 \t\n, \t\nboolean \t\n= \t\nfalse',
+      { integer = 1, boolean = false })
+  end)
+end)
+
+describe('Multiple keys', function()
+  assert_result('integer=1,boolean=false', { integer = 1, boolean = false })
+  assert_result('integer=1 , boolean=false', { integer = 1, boolean = false })
+end)
+
+describe('Edge cases', function()
+  it('Empty string', function()
+    assert_result('', {})
+  end)
+
+  it('Only commas', function()
+    assert_result(',,', {})
+  end)
+
+  it('More commas', function()
+    assert_result(',,,', {})
+  end)
+
+  it('Commas with whitespaces', function()
+    assert_result(', , ,', {})
+  end)
+
+  it('Whitespace, comma', function()
+    assert_result(' ,', {})
+  end)
+
+  it('Comma, whitespace', function()
+    assert_result(', ', {})
+  end)
+end)
+
+describe('Duplicate keys', function()
+  it('Without whitespaces', function()
+    assert_result('integer=1,integer=2', { integer = 2 })
+  end)
+
+  it('With whitespaces', function()
+    assert_result('integer=1 , integer=2', { integer = 2 })
+  end)
+end)
+
+describe('All features', function()
+  it('List of standalone strings', function()
+    assert_result('one,two,three', { 'one', 'two', 'three' },
+      { naked_as_value = true })
+  end)
+
+  it('List of standalone integers', function()
+    assert_result('1,2,3', { 1, 2, 3 })
+  end)
+
+  it('Nested tables', function()
+    assert_result('level1={level2={level3=level3}}',
+      { level1 = { level2 = { level3 = 'level3' } } })
+  end)
+
+  it('String without quotes', function()
+    assert_result('string = without \'quotes\'',
+      { string = 'without \'quotes\'' })
+  end)
+
+  it('String with quotes', function()
+    assert_result('string = "with quotes: ,={}"',
+      { string = 'with quotes: ,={}' })
+  end)
+
+  it('Negative number', function()
+    assert_result('number = -0.123', { number = -0.123 })
+  end)
+end)
+
+describe('Array', function()
+  it('Key with nested tables', function()
+    assert_result('t={a,b},z={{a,b},{c,d}}',
+      { t = { 'a', 'b' }, z = { { 'a', 'b' }, { 'c', 'd' } } },
+      { naked_as_value = true })
+  end)
+
+  it('Nested list of strings', function()
+    assert_result('{one,two,tree}', { { 'one', 'two', 'tree' } },
+      { naked_as_value = true })
+  end)
+
+  it('standalone and key value pair', function()
+    assert_result('{one,two,tree={four}}', { { 'one', 'two', tree = 'four' } },
+      { naked_as_value = true })
+  end)
+
+  it('Deeply nested string value', function()
+    assert_result('{{{one}}}', { { { { 'one' } } } },
+      { unpack = false, naked_as_value = true })
+  end)
+end)
+
+describe('Only values', function()
+  it('List of mixed values', function()
+    assert_result('-1.1,text,-1cm,True', { -1.1, 'text', '-1cm', true },
+      { naked_as_value = true })
+  end)
+
+  it('Only string values', function()
+    assert_result('one,two,three', { 'one', 'two', 'three' },
+      { naked_as_value = true })
   end)
 end)
