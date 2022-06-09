a928acc30d246f6dc9f8f02a5183f35b3ba00dfb
Josef Friedrich <josef@friedrich.rocks>
2022-06-09 20:07:51 +0200

    Extend docs

---
+++ examples/data-types/string.lua
@@ -0,0 +1,26 @@
+require('busted.runner')()
+local luakeys = require('luakeys')
+
+local kv_string = [[
+  without double quotes = no commas and equal signs are allowed,
+  with double quotes = ", and = are allowed",
+  escape quotes = "a quote \" sign",
+  curly braces = "curly { } braces are allowed",
+]]
+local result = luakeys.parse(kv_string)
+luakeys.debug(result)
+-- {
+--   ['without double quotes'] = 'no commas and equal signs are allowed',
+--   ['with double quotes'] = ', and = are allowed',
+--   ['escape quotes'] = 'a quote \" sign',
+--   ['curly braces'] = 'curly { } braces are allowed',
+-- }
+
+it('result', function()
+  assert.is.same({
+    ['without double quotes'] = 'no commas and equal signs are allowed',
+    ['with double quotes'] = ', and = are allowed',
+    ['escape quotes'] = 'a quote \\" sign',
+    ['curly braces'] = 'curly { } braces are allowed',
+  }, result)
+end)
