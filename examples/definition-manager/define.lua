8e90c26eb6cd3fd111073c945c20e602b4aa89c4
Josef Friedrich <josef@friedrich.rocks>
2025-06-30 08:55:31 +0200

    doc: add code example for the DefinitionManager

--- examples/definition-manager/define.lua
+++ examples/definition-manager/define.lua
@@ -8,3 +8,19 @@ local manager = DefinitionManager({
   key2 = { default = 2 },
   key3 = { default = 3 },
 })
+
+it('Method: define', function()
+  local parse = manager:define({
+    key1 = 'new1'
+  })
+
+  local result = parse('new1')
+  assert.are.same(result, {
+    new1 = 1 }
+  )
+
+  -- exception
+  assert.has_error(function()
+    parse('key1')
+  end, 'luakeys error [E019]: Unknown keys: “key1,”')
+end)
